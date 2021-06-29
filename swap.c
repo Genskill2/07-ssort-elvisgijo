d1ad2689b3b9da7139a057e3fdb2bbe7a86001de
Elvis Gijo Paul <84615837+elvisgijo@users.noreply.github.com>
2021-06-29 12:28:24 +0530

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,17 +1,15 @@
 void swap_max(int arr[],int l,int n) { 
-         int max = arr[n];
-         int t;
-         for(int i=n;i<l;i++) { 
-               if(arr[i]>max){
-                      int t = arr[i];
-                    }
-                 }
-            if(t>max){
-                 int c = max;
-                 max = t;
-                 t = c;
-               }
-        }
+        int maxidx;
+         for (i = n; i < l-1; i++) {
+            maxidx=i;
+            for (j = i+1; j < l; j++) {
+                   if (arr[j] > arr[maxidx]) 
+                         maxidx = j; 
+                   }
+                int t=arr[maxidx];
+                arr[i]=arr[maxidx];
+                arr[maxidx]=t;
+         }
  void ssort(int arr[],int l){
         for( int i=0;i<=l-1;i++){
                 swap_max(arr,l,i);
