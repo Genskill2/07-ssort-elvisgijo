04f6c4671a946d1c26050334cdaf547e38a51769
Elvis Gijo Paul <84615837+elvisgijo@users.noreply.github.com>
2021-06-29 12:17:13 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,20 @@
+void swap_max(int arr[],int l,int n) { 
+         int max = arr[n];
+         for(int i=n;i<l;i++) { 
+               if(arr[i]>max){
+                      int t = arr[i];
+                    }
+                 }
+            if(t>max){
+                 int c = max;
+                 max = t;
+                 t = c;
+               }
+        }
+ void ssort(int arr[],int l){
+        for( int i=0;i<=l-1;i++){
+                swap_max(arr,l,i);
+             }
+       }
+                  
+                              
